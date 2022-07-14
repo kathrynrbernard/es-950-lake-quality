4397b70b907c34181615959af13fe5cc42fb7e32
Kathryn Bernard <kathrynrbernard@gmail.com>
2022-07-14 14:55:00 -0500

    Add lintr and gridExtra

--- scripts/01_install_packages.R
+++ scripts/01_install_packages.R
@@ -2,3 +2,5 @@
 # Purpose: this script installs all packages necessary for data cleaning, analysis, and visualization
 
 install.packages("tidyverse") # used for dataframe manipulation (primarily dplyr) and visualizations (ggplot2)
+install.packages("lintr")
+install.packages("gridExtra") # used for some plotting
