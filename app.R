9d3844d454ea9c00cd44330a6ac690c969cb2bac
Kathryn Bernard <kathrynrbernard@gmail.com>
2022-07-21 15:23:56 -0500

    Erosion graph tweaks

--- app.R
+++ app.R
@@ -115,7 +115,7 @@ ui <- fluidPage(titlePanel("Big Arbor Vitae Lake Quality"), # Application title
                             ),
                   tabPanel("Erosion", 
                            fluidRow(style="padding-bottom: 50px; padding-top: 10px;",column(8, plotOutput("erosion_plot")),
-                                    column(4, "This density graph shows the amount of Rip Rap areas present on the lake.")),
+                                    column(4, "This density graph shows the length of riprap areas present on the lake.")),
                   ),
                   
                 ))) 
@@ -219,46 +219,13 @@ server <- function(input, output) {
     erosion_control <-
       select(arbor_parcel,
              c(PARCELID, VERTICAL_WALL_LEN, RIPRAP_LEN, EROSION_CNTRL_LEN))
-    erosion_risks <- select(
-      arbor_parcel,
-      c(
-        PARCELID,
-        POINT_SOURCE_PRES,
-        CHANNEL_FLOW_PRES,
-        STAIR_LAKE_PRES,
-        LAWN_LAKE_PRES,
-        SAND_DEP_PRES,
-        OTHER_RUNOFF_PRES
-      )
-    )
-    
-    erosion_risks %>% ggplot(aes(x = OTHER_RUNOFF_PRES)) +
-      geom_histogram(binwidth = 1)
-    
-    # Most parcels do not have any runoff concerns; a few have POINT_SOURCE and a few more have CHANNEL_FLOW but none have the others
-    
-    erosion_meas <-
-      select(arbor_parcel, c(PARCELID, GREAT_ERO_LEN, LESS_ERO_LEN))
-    # more parcels have GREAT_ERO_LEN than have concerns documented - could still include this
-    
-    erosion_control %>% summarize(
-      mean_wall_len = mean(VERTICAL_WALL_LEN),
-      min_wall_len = min(VERTICAL_WALL_LEN),
-      max_wall_len = max(VERTICAL_WALL_LEN),
-      mean_riprap_len = mean(RIPRAP_LEN),
-      min_riprap_len = min(RIPRAP_LEN),
-      max_riprap_len = max(RIPRAP_LEN),
-      mean_ctrl_len = mean(EROSION_CNTRL_LEN),
-      min_ctrl_len = min(EROSION_CNTRL_LEN),
-      max_ctrl_len = max(EROSION_CNTRL_LEN)
-    )
     
     erosion_control %>%
       ggplot( aes(x=RIPRAP_LEN)) +
       geom_density(fill="#741b08", color="#de4524") +
       ggtitle("Amount of Riprap in Parcels") +
-      labs(x = "Rip Rap Length", y = "Density") + 
-      theme_ipsum()
+      labs(x = "Riprap Length (Feet)", y = "Density") + 
+      theme_minimal()
   })
 }
 
