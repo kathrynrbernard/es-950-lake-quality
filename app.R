2dc43021626e1ee9722cc50684333e486deb93dd
Kathryn Bernard <kathrynrbernard@gmail.com>
2022-07-31 21:01:24 -0500

    Add land parcel drilldown and restructure text

--- app.R
+++ app.R
@@ -5,44 +5,71 @@ ui <- fluidPage(
   titlePanel("Big Arbor Vitae Lake Quality"), # Application title
   tabsetPanel(
      tabPanel("Home",
-              h2("About this Page"),
+              h2("About this page",style="text-decoration: underline;"),
               p("This website displays information about Big Arbor Vitae Lake. All the data visualized here was collected by the 
               Wisconsin Department of Natural Resources as part of their shoreline monitoring program.
               The graphs show metrics related to overall lake quality as well as data from selected parcels.
-              Where appropriate, we include recommendations for the lake association or landowners.",style="font-size:22px;"),
-              h2("Navigating the Website"),
+              Where appropriate, we include recommendations for the lake association or landowners.",style="font-size:20px;"),
+              h2("Navigating the website",style="text-decoration: underline;"),
               p("The app contains three main categories of visualizations: Land, Water, and Erosion.
               These categories correspond to the riparian zone, the littoral zone, and the bank zone of the lake.
               Each category is displayed on a separate tab within the website. Click through the tabs at the top of the page
-              to view the graphs for each category.",style="font-size:22px;"),
+              to view the graphs for each category.",style="font-size:20px;"),
+              p("Each tab has a few visualizations. Some of those visualizations are related to the lake as a whole, and some offer
+                in-depth look at a few specific parcels on the lake. We chose three parcels to examine more closely. You can follow the same three parcels
+                across the tabs to see how they compare in each shorefront zone.", style="font-size:20px;"),
               div(style="padding-top: 10px;",img(src='BigArborAerial.png', align = "center", width=600))
               ),
        tabPanel("Land",
-                h3("The Riparian Zone at a Glance"),
+                h3("The Riparian Zone at a glance",style="text-decoration: underline;"),
                 p("Let's start by taking a look at what types of land cover are present in the riparian zone of Big Arbor Vitae."
-                     ,style="font-size:22px;"),
+                     ,style="font-size:18px;"),
                fluidRow(style="padding-bottom: 50px; padding-top: 10px;",
                 column(7, plotlyOutput("land_cover_heatmap")), # need to use plotlyOutput instead of plotOutput
-                column(5, p("This heatmap shows the percentage of each type of land cover for each parcel.
+                column(5, h5("Summary"),
+                        p("This heatmap shows the percentage of each type of land cover for each parcel.
                          Hovering over a square on the heatmap will display the parcel id and percent coverage for the land cover type.
-                          There appears to be a substantial amount of shrub and herbaceous covering on Big Arbor Vitae, and not much manicured lawn.
-                          Shrubs are small-ish woody plants and herbaceous plants are things like grasses - both of these types of 
+                          There appears to be a substantial amount of shrub and herbaceous covering on Big Arbor Vitae, and not much manicured lawn."
+                          ,style="font-size:16px;"),
+                       h5("Recommendations"),
+                          p("Shrubs are small-ish woody plants and herbaceous plants are things like grasses - both of these types of 
                           land cover are beneficial for controlling erosion, and native plantings are an excellent habitat for
                           animals living near the lakeshore. Manicured lawn has the opposite effect - it can contribute to erosion and runoff
                           concerns, and doesn't provide any habitat benefits. It is great to see so much beneficial land cover present on this lake!"
-                            ,style="font-size:18px;"))),
-               h3("Breaking it Down"),
+                            ,style="font-size:16px;"))),
+               h3("Breaking it down",style="text-decoration: underline;"),
                p("We split the parcels on this lake into two categories, developed and undeveloped.
                     Developed parcels are primarily located on the south end of the lake where there is a high concentration of built structures, 
                     and undeveloped parcels are primarily located on the north end of the lake, which appears to be largely untouched. We thought
                     that there might be some interesting differences in land cover type based on whether a parcel was developed or not.",
-                 style="font-size:22px;"),
+                 style="font-size:18px;"),
                fluidRow(style="padding-bottom: 50px; padding-top: 10px;",
                  column(7, plotOutput("avg_sh_lawn_development")),
-                 column(5, p("This graph shows the average percent cover of both shrub/herbaceous covering and manicured lawn. 
-                          This graph shows that both developed and undeveloped parcels tend to have a high percent of
-                          shrub and herbaceous covering. It seems like landowners are doing a relatively good job of maintaining
-                          beneficial land cover on their properties, even in more highly developed areas.",style="font-size:18px;")))
+                 column(5, h5("Summary"),
+                        p("This graph shows the average percent cover of both shrub/herbaceous covering and manicured lawn. 
+                          Both developed and undeveloped parcels tend to have a high percent of
+                          shrub and herbaceous covering.",style="font-size:16px;"),
+                        h5("Recommendations"),
+                        p("It seems like landowners are doing a good job of maintaining
+                          beneficial land cover on their properties, even in more highly developed areas.
+                          There's still some room for improvement, though, Landowners with a lot of manicured lawn could consider
+                          re-wilding parts of their property to reintroduce native plants and more land cover that helps combat erosion."
+                          ,style="font-size:16px;"))),
+               h3("Parcels",style="text-decoration: underline;"),
+               p("Let's take a look at some information for our three parcels.",style="font-size:18px;"),
+               fluidRow(style="padding-bottom: 50px; padding-top: 10px;",
+                 column(7, plotOutput("land_cover_parcels")),
+                 column(5, h5("Summary"),
+                        p("The graph shows the percent coverage for each land cover type for each of the three
+                             parcels. The title on each graph also shows the total number of built structures in the
+                             riparian zone for each parcel. It looks like the two parcels with a high percentage of
+                             shrub and herbaceous covering don't have any built structures, while the parcel with a high
+                             percentage of manicured lawn has 8 total built structures in this zone of the shorefront.",style="font-size:16px;"),
+                        h5("Recommendations"),
+                        p("Built structures in the riparian zone, especially combined with a lot of manicured lawn and not a lot of
+                             benefical land cover, can contribute to erosion concerns. This might be a parcel to keep a closer eye on.
+                             ",style="font-size:16px;")))
+               
         ),
         tabPanel("Water",
                 fluidRow(style="padding-bottom: 50px; padding-top: 10px;",
@@ -134,6 +161,23 @@ server <- function(input, output) {
       scale_y_continuous(labels = function(x) paste0(x, "%")) + # show % signs 
       theme_minimal()  +
       theme(plot.title = element_text(hjust = 0.5,size=15))
+  })
+  
+  output$land_cover_parcels <- renderPlot({
+    
+    labels <- c(paste("Parcel ID: ",factor(parcel_dd$PARCELID), "\nTotal Structures: ", parcel_dd$STRUCTURES_TOTAL_LAND))
+    parcel_pivot %>% mutate(PARCELID=factor(parcel_pivot$PARCELID, 
+                                            levels=c("2-2649", "2-2562-02", "2-2686-16"),
+                                            labels=labels)) %>% 
+      ggplot(aes(x=Vegetation,y=Pct)) +
+      geom_bar(stat="identity", position="dodge",fill=greens15,color=greens[9]) +
+      labs(x="Type of Vegetation", y="Percent Coverage", title="Land Cover Type Per Parcel") +
+      scale_x_discrete(limits=c("SHRUB_HERB_PCT", "MANI_LAWN_PCT", "IMPERVIOUS_PCT", "AG_PCT", "OTHER_PCT"), 
+                       labels=c("Shrub/Herb", "Lawn", "Impervious", "Agriculture", "Other")) +
+      facet_wrap(facets=vars(PARCELID)) +
+      coord_flip() + 
+      theme_minimal()
+    
   })
   
   output$aquatic_veg_structures <- renderPlot({
